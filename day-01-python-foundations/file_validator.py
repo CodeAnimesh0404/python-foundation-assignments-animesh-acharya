4cccbccae6f9fd0dd99306dace0bd29afd1b9d73
CodeAnimesh0404 <acharyaanimesh00@gmail.com>
2026-08-12 20:29:15 +0545

    Added comments for clarity in file_validator.py

--- day-01-python-foundations/file_validator.py
+++ day-01-python-foundations/file_validator.py
@@ -3,11 +3,15 @@ Student: Animesh Acharya
 Day: 1
 Exercise: 3
 
+# Input values
 valid_extensions = (".csv", ".json", ".parquet")
 
+# User input
 file_name = input("Enter a file name: ")
+# Cleaning
 file_name_clean = file_name.strip().lower()
 
+# Validation and output
 if file_name_clean.endswith(valid_extensions):
     print(f"'{file_name}' is a valid file type.")
 else:
